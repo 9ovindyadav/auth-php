4afe7cf368bc5d9b61f3c74f6b7409342d97b44a
Govind yadav <govindsvyadav@gmail.com>
2023-08-31 17:52:10 +0530

    Home page

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['username'])){
+    header('location:login.php');
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1 class="mt-5 text-center">Welcome <?= $_SESSION['username'] ?>  </h1>
+    <div class="container">
+        <a href="logout.php" class="btn btn-primary" >Logout</a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
